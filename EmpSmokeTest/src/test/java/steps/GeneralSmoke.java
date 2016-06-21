0a65c679b6548b12f109e03b979a8d6a80a23f8b
Adrian Szymański <aszymanski@infusion.com>
2016-06-21 21:02:44 +0200

    Surefire, asserJ

--- EmpSmokeTest/src/test/java/steps/GeneralSmoke.java
+++ EmpSmokeTest/src/test/java/steps/GeneralSmoke.java
@@ -1,7 +1,12 @@
 package steps;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.util.VisibleForTesting;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GeneralSmoke {
@@ -14,10 +19,15 @@ public class GeneralSmoke {
 	
 	@Test(description="Open page")
 	public void setUp() {
-	driver = new ChromeDriver();
-	driver.get("http://www.wp.pl");
-	}
+		boolean check = false;
+		driver = new ChromeDriver();
+		driver.get("http://www.wp.pl");
 	
+		SoftAssertions softAssertions = new SoftAssertions();
+		softAssertions.assertThat("ss").isNotEqualTo("sqasa");
+		softAssertions.assertAll();
+	}
+
 	@Test(description="Close browser")
 	public void tearDown(){
 		driver.close();
